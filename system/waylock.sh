56e0fa2e7b9602116193e3d8a53588aac33b4d36
DemonKingSwarn <rockingswarn@gmail.com>
2022-10-06 03:05:39 +0530

    autocommit

--- system/waylock.sh
+++ system/waylock.sh
@@ -2,5 +2,4 @@
 
 
 swayidle -w \
-    timeout 600 'systemctl suspend'
-    before-sleep 'swaylock -i $HOME/hdd/pix/wall/01.jpg --ring-color 8218c4 --key-hl-color 3e0c60'
+    timeout 600 'swaylock -i $HOME/hdd/pix/wall/01.jpg --ring-color 8218c4 --key-hl-color 3e0c60'
