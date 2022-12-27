eb9b136bde2302e8f0affcf290772f9530eb1f5c
DemonKingSwarn <rockingswarn@gmail.com>
2022-12-27 21:18:18 +0530

    autocommit

--- misc/brightness.sh
+++ misc/brightness.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 	
-exec doas sh -c "cat /sys/class/backlight/intel_backlight/max_brightness > /sys/class/backlight/intel_backlight/brightness"
-
+#exec doas sh -c "cat /sys/class/backlight/intel_backlight/max_brightness > /sys/class/backlight/intel_backlight/brightness"
+printf "%s" "Enter the brightness amount: " && read -r bright
+echo "$bright" | sudo tee /sys/class/backlight/intel_backlight/brightness 
 
