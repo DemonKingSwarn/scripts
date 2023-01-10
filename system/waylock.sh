4d85e8dc82e58f9a1b52dffef67dea48f248687a
DemonKingSwarn1 <bca40517.22@bitmesra.ac.in>
2023-01-10 21:35:35 +0530

    autocommit

--- system/waylock.sh
+++ system/waylock.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 
-swayidle -w \
-    timeout 600 'swaylock -i $HOME/pix/wall/93.jpg --ring-color 8218c4 --key-hl-color 3e0c60'
+ICON=$HOME/.config/i3/lock.png
+IMAGEDIR=/tmp/uwu.png
+grim $IMAGEDIR
+convert $IMAGEDIR -scale 10% -scale 1000% $IMAGEDIR
+convert $IMAGEDIR $ICON -gravity center -composite -matte $IMAGEDIR
+
+#swayidle -w \
+#    timeout 600 'swaylock -i $IMAGEDIR --ring-color 8218c4 --key-hl-color 3e0c60'
+swaylock -u -i $IMAGEDIR
+rm -rf /tmp/uwu.png
