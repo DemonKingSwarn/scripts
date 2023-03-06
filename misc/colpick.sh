eb58982e9c20efadcd053f4b1c9fb9161e404ae0
DemonKingSwarn <rockingswarn@gmail.com>
2023-03-06 12:48:24 +0530

    autocommit
     an automated commit

--- misc/colpick.sh
+++ misc/colpick.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-var=$(hyprpicker | awk '{print $1}')
+var=$(hyprpicker)
 echo "$var"
 echo "$var" | tr -d '\n' | wl-copy
 notify-send " " "<span color='$var' font='26px'><b>$var</b></span><span font='20'>Copied to clipboard</span>."
