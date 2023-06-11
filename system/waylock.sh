c699ba773cc0de66909ffd049bff3325dfbb395e
DemonKingSwarn <rockingswarn@gmail.com>
2023-06-12 02:46:39 +0530

    some stuff working haha
     an automated commit

--- system/waylock.sh
+++ system/waylock.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+cleanup(){
+    rm -rf $IMAGEDIR
+}
+
 
 ICON=$HOME/.config/i3/lock.png
 IMAGEDIR=/tmp/uwu.png
@@ -10,4 +14,4 @@ convert $IMAGEDIR $ICON -gravity center -composite -matte $IMAGEDIR
 #swayidle -w \
 #    timeout 600 'swaylock -i $IMAGEDIR --ring-color 8218c4 --key-hl-color 3e0c60'
 swaylock -u -i $IMAGEDIR
-rm -rf /tmp/uwu.png
+trap cleanup EXIT INT TERM
