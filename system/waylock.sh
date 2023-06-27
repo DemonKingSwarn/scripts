905161ec74bfe8190d77b0dbb72c5e9fbb2b2346
DemonKingSwarn <rockingswarn@gmail.com>
2023-06-27 14:52:34 +0530

    This is where it all begins...
     an automated commit

--- system/waylock.sh
+++ system/waylock.sh
@@ -13,5 +13,6 @@ convert $IMAGEDIR $ICON -gravity center -composite -matte $IMAGEDIR
 
 #swayidle -w \
 #    timeout 600 'swaylock -i $IMAGEDIR --ring-color 8218c4 --key-hl-color 3e0c60'
-swaylock -u -i $IMAGEDIR
+swaylock -u -i $IMAGEDIR 
+
 trap cleanup EXIT INT TERM
