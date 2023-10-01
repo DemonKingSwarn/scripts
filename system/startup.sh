a60355f7849cc5d3506ac84569cde3050388ed8f
DemonKingSwarn <rockingswarn@gmail.com>
2023-10-01 18:34:41 +0530

    It's 2016; why are we using ColdFusion?!
     an automated commit

--- system/startup.sh
+++ system/startup.sh
@@ -3,13 +3,13 @@
 # This is the startup script.
 
 # cursor settings
-xsetroot -cursor_name left_ptr 
+xsetroot -cursor_name 'Bibata-Modern-Amber'
 #xinput set-prop "Logitech G102 LIGHTSYNC Gaming Mouse" "Coordinate Transformation Matrix" 0.7 0 0 0 0.7 0 0 0 1
 xset r rate 300 50
 
 # keyboard settings
 ## Turns Caps Lock into Control, useful for Emacs
-setxkbmap -option ctrl:nocaps
+#setxkbmap -option ctrl:nocaps
 sxhkd &
 
 # system
@@ -23,15 +23,11 @@ udiskie &
 # rice
 xrdb -merge ~/.config/X11/xresources &
 #picom --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350 --experimental-backends -b &
-picom --experimental-backends -b &
+picom -b &
 dunst &
 $HOME/.scripts/setbg/setbg &
-$HOME/.config/polybar/launch.sh &
-bspswallow &
-xfdesktop &
-
-#Updates dotfiles
-# $HOME/.scripts/misc/autocommitpressure &
+#$HOME/.config/polybar/launch.sh &
+#bspswallow &
 
 # D-EMACS
 #emacs --daemon &
