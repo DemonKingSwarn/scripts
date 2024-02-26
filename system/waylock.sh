ea2a966ce3be8f83b38cb14a9ef18b266d053318
DemonKingSwarn <rockingswarn@gmail.com>
2024-02-26 12:45:58 +0530

    I would rather be playing SC2.
     an automated commit

--- system/waylock.sh
+++ system/waylock.sh
@@ -5,14 +5,9 @@ cleanup(){
 }
 
 
-ICON=$HOME/.config/i3/lock.png
 IMAGEDIR=/tmp/uwu.png
 grim $IMAGEDIR
 convert $IMAGEDIR -scale 10% -scale 1000% $IMAGEDIR
-convert $IMAGEDIR $ICON -gravity center -composite -matte $IMAGEDIR
-
-#swayidle -w \
-#    timeout 600 'swaylock -i $IMAGEDIR --ring-color 8218c4 --key-hl-color 3e0c60'
-swaylock -u -i $IMAGEDIR 
+hyprlock
 
 trap cleanup EXIT INT TERM
