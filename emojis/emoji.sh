7125919caecd62c955105dfed9a1ba66a2e56e75
DemonKingSwarn <rockingswarn@gmail.com>
2024-11-21 18:22:27 +0530

    Does not work.
     an automated commit

--- emojis/emoji.sh
+++ emojis/emoji.sh
@@ -4,11 +4,13 @@
 
 # lol
 DIRNAME=$HOME/.scripts/emojis/
+RECENTS=$DIRNAME/recent.txt
 
 chosen=$(cut -d ';' -f1 "$DIRNAME/emoji.txt" | rofi -dmenu | sed "s/ .*//")
 
 [ -z "$chosen" ] && exit
 
 printf "$chosen" | wl-copy
+wl-paste >> $RECENTS
 notify-send "$chosen  copied to clipboard"
 
